74899cdf479bc9641af93be886b0105924cc74ce
Rafael <rafaelmiranda178@gmail.com>
2020-02-01 09:31:03 -0300

    Criado consumer 2

--- java-consumer/src/main/java/com/fiap/integrationtools/consumer/recipient/RecipientConsumer.java
+++ java-consumer/src/main/java/com/fiap/integrationtools/consumer/recipient/RecipientConsumer.java
@@ -11,8 +11,41 @@ import org.springframework.messaging.handler.annotation.Payload;
 @EnableBinding(RecipientChannel.class)
 public class RecipientConsumer {
 
+    private Recipient recHighestInstallment;
+
     @StreamListener(target = RecipientChannel.INPUT)
     public void handle(@Payload Recipient message) {
-        log.info("Should process by recipient " + message);
+        if(recHighestInstallment == null){
+            recHighestInstallment = message;
+            printRecipient(recHighestInstallment);
+        }else if(message.getInstallmentValue() > recHighestInstallment.getInstallmentValue()){
+            recHighestInstallment = message;
+            printRecipient(recHighestInstallment);
+        }
     }
+
+    private void printRecipient(Recipient recipient){
+        StringBuilder builder = new StringBuilder();
+        builder.append("\n\n\n");
+        builder.append("************ Highest Installment Value ************\n");
+        builder.append("NIS_Favorecido: ");
+        builder.append(recipient.getRecipientNis());
+        builder.append("\n");
+        builder.append("NOME_FAVORECIDO: ");
+        builder.append(recipient.getRecipientName());
+        builder.append("\n");
+        builder.append("VALOR_PARCELA: ");
+        builder.append(recipient.getInstallmentValue());
+        builder.append("\n");
+        builder.append("NOME_MUNICIPIO: ");
+        builder.append(recipient.getCityName());
+        builder.append("\n");
+        builder.append("UF: ");
+        builder.append(recipient.getUf());
+        builder.append("\n");
+        builder.append("----------------------------------------------");
+        builder.append("\n\n\n");
+        log.info(builder.toString());
+    }
+
 }
